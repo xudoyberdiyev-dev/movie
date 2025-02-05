5e32dbe893117dc3d33d9cb433ecd194fae54a25
Azizbek <132741338+Azizbek-dev-ful@users.noreply.github.com>
2025-02-05 22:14:55 +0500

    v1

--- movie-client/src/components/Search.jsx
+++ movie-client/src/components/Search.jsx
@@ -1,8 +1,8 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import searchIcon from "../assets/images/search.png"; // search rasm
 import closeIcon from "../assets/images/close.png";  // close rasm
-import {GetAuto} from "../service/userService/AppService.js";
-import {APP_API} from "../service/AppApi.js";
+import { GetAuto } from "../service/userService/AppService.js";
+import { APP_API } from "../service/AppApi.js";
 import "./search.css";
 
 export const Search = () => {
@@ -12,7 +12,6 @@ export const Search = () => {
 
     // Ovozli qidiruvni boshlash
     const startVoiceSearch = () => {
-        // SpeechRecognition funksiyasini tekshirish
         const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
         if (!SpeechRecognition) {
@@ -38,7 +37,6 @@ export const Search = () => {
 
         recognition.start();
     };
-
 
     // Qidiruvni amalga oshirish
     const performSearch = async (query) => {
@@ -91,13 +89,33 @@ export const Search = () => {
                         className="loading-icon"
                     />
                 </div>
-                {/*<button onClick={() => performSearch(searchQuery)}>Qidirish</button>*/}
-                {/*<button onClick={startVoiceSearch}>Ovoz bn</button>*/}
-                {/*<button onClick={() => setIsModalOpen(true)}>d</button>*/}
+                <button onClick={() => performSearch(searchQuery)}>Qidirish</button>
+                <button onClick={startVoiceSearch}>Ovoz bn</button>
                 <button className="search-btn">
                     <img src={closeIcon} width="24" height="24" alt="close search box"/>
                 </button>
             </div>
+
+            {/* Modal yoki natijalarni ko'rsatish */}
+            {isModalOpen && (
+                <div className="movie-results">
+                    <button onClick={closeModal}>Yopish</button>
+                    <h3>Natijalar:</h3>
+                    <div className="movie-list">
+                        {movies.length > 0 ? (
+                            movies.map((movie) => (
+                                <div key={movie.id} className="movie-item">
+                                    <h4>{movie.name}</h4>
+                                    <p>{movie.description}</p>
+                                    {/* Agar kinoning boshqa tafsilotlari kerak bo'lsa, ularni ham qo'shish mumkin */}
+                                </div>
+                            ))
+                        ) : (
+                            <p>Hech narsa topilmadi.</p>
+                        )}
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
