885371fccc3ab6c6ede1ac0028969da92b6053cb
Azizbek <132741338+Azizbek-dev-ful@users.noreply.github.com>
2025-02-24 00:40:01 +0500

    v1

--- movie-service/src/main/java/it/movie/movie_animation/bot/BotConfig.java
+++ movie-service/src/main/java/it/movie/movie_animation/bot/BotConfig.java
@@ -1,20 +1,6 @@
-//package it.movie.movie_animation.bot;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.stereotype.Service;
-//import org.telegram.telegrambots.TelegramBotsApi;
-//import org.telegram.telegrambots.exceptions.TelegramApiException;
-//import org.telegram.telegrambots.generics.LongPollingBot;
-//
-//@Service
-//public class BotConfig {
-//    public BotConfig(LongPollingBot bot) {
-//        TelegramBotsApi botsApi = new TelegramBotsApi();
-//        try {
-//            botsApi.registerBot(bot);
-//        } catch (TelegramApiException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//}
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BotConfig {
+
+}
--- movie-service/src/main/java/it/movie/movie_animation/bot/TelegramBotService.java
+++ movie-service/src/main/java/it/movie/movie_animation/bot/TelegramBotService.java
@@ -1,71 +1,49 @@
-//package it.movie.movie_animation.bot;
-//
-//import org.hibernate.sql.Update;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.stereotype.Component;
-//import org.springframework.web.client.RestTemplate;
-//import org.telegram.telegrambots.api.methods.send.SendMessage;
-//import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-//import org.telegram.telegrambots.exceptions.TelegramApiException;
-//
-//@Component
-//public class TelegramBotService extends TelegramLongPollingBot {
-//    @Value("${telegram.bot.token}")
-//    private String botToken;
-//
-//    @Value("${telegram.bot.username}")
-//    private String botUsername;
-//
-//    @Value("${telegram.channel.id}")
-//    private String channelId;
-//
-//
-//    @Override
-//    public void onUpdateReceived(org.telegram.telegrambots.api.objects.Update update) {
-//        if (update.hasMessage() && update.getMessage().hasText()) {
-//            Integer userId = update.getMessage().getFrom().getId();
-//            String chatId = update.getMessage().getChatId().toString();
-//
-//            if (update.getMessage().getText().equals("/start")) {
-//                if (isUserSubscribed(userId)) {
-//                    sendMessage(chatId, "✅ Siz kanalga a'zo bo‘lgansiz! Endi botdan foydalanishingiz mumkin.");
-//                } else {
-//                    sendMessage(chatId, "🚀 Botdan foydalanish uchun kanalga a'zo bo‘ling!\n\n👉 " + channelId);
-//                }
-//            }
-//        }
-//    }
-//
-//    @Override
-//    public String getBotUsername() {
-//        return botUsername;
-//    }
-//
-//    @Override
-//    public String getBotToken() {
-//        return botToken;
-//    }
-//
-//    private final RestTemplate restTemplate = new RestTemplate();
-//
-//
-//    private boolean isUserSubscribed(Integer userId) {
-//        String url = String.format("https://api.telegram.org/bot%s/getChatMember?chat_id=%s&user_id=%d", botToken, channelId, userId);
-//        try {
-//            return restTemplate.getForObject(url, String.class).contains("\"status\":\"member\"");
-//        } catch (Exception e) {
-//            return false;
-//        }
-//    }
-//
-//    private void sendMessage(String chatId, String text) {
-//        SendMessage message = new SendMessage();
-//        message.setChatId(chatId);
-//        message.setText(text);
-//        try {
-//            execute(message);
-//        } catch (TelegramApiException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//}
+package it.movie.movie_animation.bot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Service
+public class TelegramBotService extends TelegramLongPollingBot {
+    @Value("${telegram.bot.username}")
+    private String botUsername;
+
+    @Value("${telegram.bot.token}")
+    private String botToken;
+
+    @Override
+    public String getBotUsername() {
+        return botUsername;
+    }
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+
+            if (messageText.equals("/start")) {
+                sendMessage(chatId, "Salom!");
+            }
+        }
+    }
+    private void sendMessage(long chatId, String text) {
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(text);
+
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
