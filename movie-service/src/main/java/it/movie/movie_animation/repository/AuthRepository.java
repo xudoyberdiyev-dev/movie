97167e1b078d8e90b106d6e888394cfe6bd3cf00
Azizbek <132741338+Azizbek-dev-ful@users.noreply.github.com>
2025-02-24 23:27:20 +0500

    v1

--- movie-service/src/main/java/it/movie/movie_animation/repository/AuthRepository.java
+++ movie-service/src/main/java/it/movie/movie_animation/repository/AuthRepository.java
@@ -11,7 +11,6 @@ import java.util.Optional;
 import java.util.UUID;
 
 @CrossOrigin
-@Repository
 public interface AuthRepository extends JpaRepository<Users, UUID> {
     UserDetails findByEmail(String email);
 
